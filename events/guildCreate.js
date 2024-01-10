6105e9fdda4d3a1431bc60129dfaef907ec0e593
SynzDevelopment <153457792+SynzDevelopment@users.noreply.github.com>
2024-01-10 23:33:37 +0000

    Debugging

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,23 +6,30 @@ module.exports = {
   once: false,
   async execute(guild) {
     try {
+      console.log(`Bot joined guild: ${guild.name} (${guild.id})`);
+
       let guildProfile = await guildData.findOne({
         id: guild.id
       });
 
       if (!guildProfile) {
+        console.log('Creating a new profile for the guild...');
         guildProfile = new guildData({
           id: guild.id,
           name: guild.name,
           verification: false
         });
         await guildProfile.save();
+        console.log('Guild profile saved.');
       }
 
-      const textChannel = await guild.channels.fetch().then(channels => channels.find(channel => channel.type === 'GUILD_TEXT' && channel.permissionsFor(guild.me).has('SEND_MESSAGES')));
+      const textChannels = await guild.channels.fetch().then(channels => channels.filter(channel => channel.type === 'GUILD_TEXT'));
+      const textChannel = textChannels.find(channel => channel.permissionsFor(guild.me).has('SEND_MESSAGES' || 'ADMINISTRATOR'));
 
       if (textChannel) {
+        console.log(`Sending welcome message in text channel: ${textChannel.name} (${textChannel.id})`);
         await textChannel.send('Thanks for adding me to your server!');
+        console.log('Welcome message sent successfully.');
       } else {
         console.error('Bot does not have permission to send messages in any text channel.');
       }

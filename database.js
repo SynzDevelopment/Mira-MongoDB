e300a91a8a79b9c8ee53585663707e439bcc8c18
SynzDevelopment <153457792+SynzDevelopment@users.noreply.github.com>
2024-01-10 02:51:39 +0000

    Fixed errors

--- database.js
+++ database.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mariadb = require('mariadb');
 
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = process.env();
+
 const pool = mariadb.createPool({
   host: DB_HOST, 
   user: DB_USER,

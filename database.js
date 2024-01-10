b1c370bfaa4062d66ca5d52446add0888234944c
SynzDevelopment <153457792+SynzDevelopment@users.noreply.github.com>
2024-01-10 02:48:48 +0000

    Re did mariadb

--- database.js
+++ database.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
-const mariadb = require("mariadb");
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+require('dotenv').config();
+const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: DB_HOST,
+  host: DB_HOST, 
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+  port: DB_PORT,
   connectionLimit: 10,
 });
 
